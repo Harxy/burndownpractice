4145ba9364dff1f9390a2fa97908cad529a9b2bf
Harxy <harkerp@gmail.com>
2016-02-27 14:30:19 +0000

    squashed a mongo error, cosmetic changes

--- lib/app.rb
+++ lib/app.rb
@@ -63,9 +63,11 @@ end
 
 post '/signin' do
   user = User.find_by(username: params['username']) if User.find_by(username: params['username'])
-  if user.password == BCrypt::Engine.hash_secret(params[:password], user.password_salt)
-    session[:user_id] = user._id
-    session[:user_name] = user.username
+  if user
+    if user.password == BCrypt::Engine.hash_secret(params[:password], user.password_salt)
+      session[:user_id] = user._id
+      session[:user_name] = user.username
+    end
   end
   redirect to('/index')
 end
@@ -76,34 +78,34 @@ get "/signout" do
   redirect "/signin"
 end
 
-  post '/graph/:id' do
-    authenticate!
-    graph = Graph.find(params[:id])
-    time = Date.today.strftime("%F")
-    if graph.daily_wordcount[time]
-      graph.daily_wordcount[time] += params['wordcount'].to_i
-    else
-      graph.daily_wordcount[time] = params['wordcount'].to_i
-    end
-    graph.save
-    redirect to('/graph/' + graph.id)
+post '/graph/:id' do
+  authenticate!
+  graph = Graph.find(params[:id])
+  time = Date.today.strftime("%F")
+  if graph.daily_wordcount[time]
+    graph.daily_wordcount[time] += params['wordcount'].to_i
+  else
+    graph.daily_wordcount[time] = params['wordcount'].to_i
   end
+  graph.save
+  redirect to('/graph/' + graph.id)
+end
 
-  get '/new' do
-    authenticate!
-    @graphs = Graph.where(user: session[:user_id].to_s)
-    erb :new
-  end
+get '/new' do
+  authenticate!
+  @graphs = Graph.where(user: session[:user_id].to_s)
+  erb :new
+end
 
-  post '/new' do
-    authenticate!
-    graph = Graph.create(
-      user: session[:user_id],
-      date_started: Date.today.strftime("%F"),
-      name: params['name'],
-      wordcount: params['wordcount'].to_i,
-      days: params['days'].to_i,
-      daily_wordcount: {}
-    )
-    redirect to('/graph/' + graph.id)
-  end
+post '/new' do
+  authenticate!
+  graph = Graph.create(
+    user: session[:user_id],
+    date_started: Date.today.strftime("%F"),
+    name: params['name'],
+    wordcount: params['wordcount'].to_i,
+    days: params['days'].to_i,
+    daily_wordcount: {}
+  )
+  redirect to('/graph/' + graph.id)
+end
