4cf299a3df9e1d7ccaf82598ba57f3c00a0d1592
Harxy <harkerp@gmail.com>
2016-02-27 15:46:35 +0000

    add overview feature

--- lib/build_graph.rb
+++ lib/build_graph.rb
@@ -10,6 +10,19 @@ class BuildGraph
     date = graph.date_started
     cumulative_wordcount = graph.wordcount
     data[(date - 1).strftime("%F")] = cumulative_wordcount
+    while date <= Date.today do
+      cumulative_wordcount -= graph.wordcount_per_day
+      data[date.strftime("%F")] = cumulative_wordcount
+      date += 1
+    end
+    data
+  end
+
+  def full_burndown_data
+    data = {}
+    date = graph.date_started
+    cumulative_wordcount = graph.wordcount
+    data[(date - 1).strftime("%F")] = cumulative_wordcount
     while cumulative_wordcount > graph.wordcount_per_day do
       cumulative_wordcount -= graph.wordcount_per_day
       data[date.strftime("%F")] = cumulative_wordcount
